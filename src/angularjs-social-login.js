b4acf897de6129dfb57f353b50cb4a5f93258a68
Alexandr Polyakov <alexandr.p@cronix.ms>
2017-07-05 13:13:48 +0300

    Rewrite in ES6

--- src/angularjs-social-login.js
+++ src/angularjs-social-login.js
@@ -1,219 +1,207 @@
-const socialLogin = angular.module('socialLogin', []);
+const socialLogin = angular.module('socialLogin', [])
 
-socialLogin.provider("social", function(){
-	var fbKey, fbApiV, googleKey, linkedInKey;
+socialLogin.provider('social', function () {
+	let fbKey, fbApiV, googleKey, linkedInKey
 	return {
-		setFbKey: function(obj){
-			fbKey = obj.appId;
-			fbApiV = obj.apiVersion;
-			var d = document, fbJs, id = 'facebook-jssdk', ref = d.getElementsByTagName('script')[0];
-			fbJs = d.createElement('script'); 
-			fbJs.id = id; 
-			fbJs.async = true;
-			fbJs.src = "//connect.facebook.net/en_US/sdk.js";
-
-			fbJs.onload = function() {
+		setFbKey: (obj) => {
+			let appId = fbKey = obj.appId
+			let version = fbApiV = obj.apiVersion
+      let ref = document.getElementsByTagName('script')[0]
+			let fbJs = document.createElement('script')
+			fbJs.id = 'facebook-jssdk'
+			fbJs.async = true
+			fbJs.src = '//connect.facebook.net/en_US/sdk.js'
+			fbJs.onload = () => {
 				FB.init({ 
-					appId: fbKey,
+					appId,
 					status: true, 
 					cookie: true, 
 					xfbml: true,
-					version: fbApiV
-				});
-	        };
-
-			ref.parentNode.insertBefore(fbJs, ref);
+					version
+				})
+			}
+			ref.parentNode.insertBefore(fbJs, ref)
 		},
-		setGoogleKey: function(value){
-			googleKey = value;
-			var d = document, gJs, ref = d.getElementsByTagName('script')[0];
-			gJs = d.createElement('script');
+		setGoogleKey: (client_id) => {
+			googleKey = client_id
+      let ref = document.getElementsByTagName('script')[0]
+			let gJs = document.createElement('script')
 			gJs.async = true;
-			gJs.src = "//apis.google.com/js/platform.js"
-
-			gJs.onload = function() {
-				var params ={
-					client_id: value,
-					scope: 'email'
-				}
-				gapi.load('auth2', function() {
-        			gapi.auth2.init(params);
-      			});
-		    };
-
-		    ref.parentNode.insertBefore(gJs, ref);
-		},
-		setLinkedInKey: function(value){
-			linkedInKey = value;
-			var lIN, d = document, ref = d.getElementsByTagName('script')[0];
-			lIN = d.createElement('script');
-			lIN.async = false;
-			lIN.src = "//platform.linkedin.com/in.js";
-			lIN.text = ("api_key: " + linkedInKey).replace("\"", "");
-	        ref.parentNode.insertBefore(lIN, ref);
-	    },
-		$get: function(){
-			return{
-				fbKey: fbKey,
-				googleKey: googleKey,
-				linkedInKey: linkedInKey,
-				fbApiV: fbApiV
+			gJs.src = '//apis.google.com/js/platform.js'
+			gJs.onload = () => {
+				gapi.load('auth2', () => {
+					gapi.auth2.init({client_id, scope: 'email'})
+        })
 			}
-		}
+      ref.parentNode.insertBefore(gJs, ref)
+		},
+		setLinkedInKey: (value) => {
+      linkedInKey = value
+      let ref = document.getElementsByTagName('script')[0]
+			let lIN = document.createElement('script')
+			lIN.async = false
+			lIN.src = '//platform.linkedin.com/in.js'
+			lIN.text = `api_key: ${linkedInKey.replace('"', '')}`
+			ref.parentNode.insertBefore(lIN, ref)
+		},
+		$get: () => ({ fbKey, googleKey, linkedInKey, fbApiV })
 	}
-});
+})
 
 /* @ngInject */
-socialLogin.factory("socialLoginService", function($window, $rootScope){
+socialLogin.factory('socialLoginService', function ($window, $rootScope) {
 	return {
-		logout: function(){
-			var provider = $window.localStorage.getItem('_login_provider');
+		logout: () => {
+			let provider = $window.localStorage.getItem('_login_provider')
 			switch(provider) {
-				case "google":
+				case 'google':
 					//its a hack need to find better solution.
-					var gElement = document.getElementById("gSignout");
-					if (typeof(gElement) != 'undefined' && gElement != null)
-					{
-					  gElement.remove();
+					let gElement = document.getElementById('gSignout')
+					if (typeof(gElement) !== 'undefined' && gElement != null) {
+					  gElement.remove()
 					}
-					var d = document, gSignout, ref = d.getElementsByTagName('script')[0];
-					gSignout = d.createElement('script');
-					gSignout.src = "https://accounts.google.com/Logout";
-					gSignout.type = "text/html";
-					gSignout.id = "gSignout";
-					$window.localStorage.removeItem('_login_provider');
-					$rootScope.$broadcast('event:social-sign-out-success', "success");
-					ref.parentNode.insertBefore(gSignout, ref);
-			        break;
-				case "linkedIn":
-					IN.User.logout(function(){
+					let ref = d.getElementsByTagName('script')[0]
+					let gSignout = d.createElement('script')
+					gSignout.src = 'https://accounts.google.com/Logout'
+					gSignout.type = 'text/html'
+					gSignout.id = 'gSignout'
+					$window.localStorage.removeItem('_login_provider')
+					$rootScope.$broadcast('event:social-sign-out-success', 'success')
+					ref.parentNode.insertBefore(gSignout, ref)
+					break
+				case 'linkedIn':
+					IN.User.logout(_ => {
+						$window.localStorage.removeItem('_login_provider')
+					 	$rootScope.$broadcast('event:social-sign-out-success', 'success')
+					}, {})
+					break
+				case 'facebook':
+					FB.logout(_ => {
 						$window.localStorage.removeItem('_login_provider');
-					 	$rootScope.$broadcast('event:social-sign-out-success', "success");
-					}, {});
-					break;
-				case "facebook":
-					FB.logout(function(res){
-						$window.localStorage.removeItem('_login_provider');
-					 	$rootScope.$broadcast('event:social-sign-out-success', "success");
-					});
-					break;
+					 	$rootScope.$broadcast('event:social-sign-out-success', 'success');
+					})
+					break
 			}
 		},
-		setProvider: function(provider){
-			$window.localStorage.setItem('_login_provider', provider);
+		setProvider: (provider) => {
+			$window.localStorage.setItem('_login_provider', provider)
 		}
 	}
-});
+})
 
 /* @ngInject */
-socialLogin.directive("linkedIn",
-	function($rootScope, social, socialLoginService, $window){
+socialLogin.directive('linkedIn', function ($rootScope, social, socialLoginService) {
 	return {
 		restrict: 'EA',
 		scope: {},
-		link: function(scope, ele, attr){
-		    ele.on("click", function(){
-		  		IN.User.authorize(function(){
-					IN.API.Raw("/people/~:(id,first-name,last-name,email-address,picture-url)").result(function(res){
-						socialLoginService.setProvider("linkedIn");
-						var userDetails = {name: res.firstName + " " + res.lastName, email: res.emailAddress, uid: res.id, provider: "linkedIN", imageUrl: res.pictureUrl};
-						$rootScope.$broadcast('event:social-sign-in-success', userDetails);
-				    });
-				});
+		link: (scope, elem, attr) => {
+		    elem.on('click', () => {
+		  		IN.User.authorize(() => {
+					IN.API.Raw('/people/~:(id,first-name,last-name,email-address,picture-url)')
+						.result(res => {
+							socialLoginService.setProvider('linkedIn')
+							let userDetails = {
+								name: `${res.firstName} ${res.lastName}`,
+								email: res.emailAddress,
+								uid: res.id,
+								provider: 'linkedIN',
+								imageUrl: res.pictureUrl
+							}
+							$rootScope.$broadcast('event:social-sign-in-success', userDetails);
+						})
+					})
+				})
+		}
+	}
+})
+
+/* @ngInject */
+socialLogin.directive('gLogin', function ($rootScope, social, socialLoginService) {
+	return {
+		restrict: 'EA',
+		scope: {},
+		replace: true,
+		link: (scope, elem, attr) => {
+			elem.on('click', () => {
+				let fetchUserDetails = () => {
+					let currentUser = scope.gauth.currentUser.get()
+					let profile = currentUser.getBasicProfile()
+					let idToken = currentUser.getAuthResponse().id_token
+					let token = currentUser.getAuthResponse().access_token
+					return {
+						token,
+						idToken,
+						name: profile.getName(), 
+						email: profile.getEmail(), 
+						uid: profile.getId(), 
+						provider: 'google',
+						imageUrl: profile.getImageUrl()
+					}
+				}
+
+				if (typeof(scope.gauth) === 'undefined') {
+          scope.gauth = gapi.auth2.getAuthInstance()
+        }
+				if (!scope.gauth.isSignedIn.get()) {
+					scope.gauth.signIn().then(googleUser => {
+						socialLoginService.setProvider('google')
+						$rootScope.$broadcast('event:social-sign-in-success', fetchUserDetails())
+					}).catch(err => console.log(err))
+				} else {
+					socialLoginService.setProvider('google')
+					$rootScope.$broadcast('event:social-sign-in-success', fetchUserDetails())
+				}
 			})
 		}
 	}
 })
 
 /* @ngInject */
-socialLogin.directive("gLogin",
-	function($rootScope, social, socialLoginService){
+socialLogin.directive('fbLogin', function ($rootScope, social, socialLoginService, $q) {
 	return {
 		restrict: 'EA',
 		scope: {},
 		replace: true,
-		link: function(scope, ele, attr){
-			ele.on('click', function(){
-				var fetchUserDetails = function(){
-					var currentUser = scope.gauth.currentUser.get();
-					var profile = currentUser.getBasicProfile();
-					var idToken = currentUser.getAuthResponse().id_token;
-					var accessToken = currentUser.getAuthResponse().access_token;
-					return {
-						token: accessToken,
-						idToken: idToken, 
-						name: profile.getName(), 
-						email: profile.getEmail(), 
-						uid: profile.getId(), 
-						provider: "google", 
-						imageUrl: profile.getImageUrl()
-					}
-				}
-		    	if(typeof(scope.gauth) == "undefined")
-		    		scope.gauth = gapi.auth2.getAuthInstance();
-				if(!scope.gauth.isSignedIn.get()){
-					scope.gauth.signIn().then(function(googleUser){
-						socialLoginService.setProvider("google");
-						$rootScope.$broadcast('event:social-sign-in-success', fetchUserDetails());
-					}, function(err){
-						console.log(err);
-					});
-				}else{
-					socialLoginService.setProvider("google");
-					$rootScope.$broadcast('event:social-sign-in-success', fetchUserDetails());
-				}
-	        	
-	        });
-		}
-	}
-});
-
-/* @ngInject */
-socialLogin.directive("fbLogin",
- function($rootScope, social, socialLoginService, $q){
-	return {
-		restrict: 'EA',
-		scope: {},
-		replace: true,
-		link: function(scope, ele, attr){
-			ele.on('click', function(){
-				var fetchUserDetails = function(){
-					var deferred = $q.defer();
-					FB.api('/me?fields=name,email,picture', function(res){
-						if(!res || res.error){
+		link: (scope, elem, attr) => {
+			elem.on('click', () => {
+				let fetchUserDetails = () => {
+					let deferred = $q.defer()
+					FB.api('/me?fields=name,email,picture', res => {
+						if (!res || res.error) {
 							deferred.reject('Error occured while fetching user details.');
-						}else{
+						} else {
 							deferred.resolve({
 								name: res.name, 
 								email: res.email, 
 								uid: res.id, 
-								provider: "facebook", 
+								provider: 'facebook',
 								imageUrl: res.picture.data.url
-							});
+							})
 						}
-					});
-					return deferred.promise;
+					})
+					return deferred.promise
 				}
-				FB.getLoginStatus(function(response) {
-					if(response.status === "connected"){
-						fetchUserDetails().then(function(userDetails){
-							userDetails["token"] = response.authResponse.accessToken;
-							socialLoginService.setProvider("facebook");
-							$rootScope.$broadcast('event:social-sign-in-success', userDetails);
-						});
-					}else{
-						FB.login(function(response) {
-							if(response.status === "connected"){
-								fetchUserDetails().then(function(userDetails){
-									userDetails["token"] = response.authResponse.accessToken;
-									socialLoginService.setProvider("facebook");
-									$rootScope.$broadcast('event:social-sign-in-success', userDetails);
-								});
+
+				FB.getLoginStatus( response => {
+					if (response.status === 'connected') {
+						fetchUserDetails().then(userDetails => {
+							userDetails.token = response.authResponse.accessToken
+							socialLoginService.setProvider('facebook')
+							$rootScope.$broadcast('event:social-sign-in-success', userDetails)
+						})
+					} else {
+						FB.login(response => {
+							if (response.status === 'connected') {
+								fetchUserDetails().then(userDetails => {
+                  userDetails.token = response.authResponse.accessToken
+                  socialLoginService.setProvider('facebook')
+                  $rootScope.$broadcast('event:social-sign-in-success', userDetails)
+								})
 							}
-						}, {scope: 'email', auth_type: 'rerequest'});
+						}, {scope: 'email', auth_type: 'rerequest'})
 					}
-				});
-			});
+				})
+			})
 		}
 	}
 })
